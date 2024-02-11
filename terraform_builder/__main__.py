6d363396f0ffca0b29890140370ef666319c5b0b
JG <moarpheus@gmail.com>
2024-02-11 09:15:46 +0200

    working stack

--- terraform_builder/__main__.py
+++ terraform_builder/__main__.py
@@ -2,6 +2,8 @@ from constructs import Construct
 from cdktf import App, NamedRemoteWorkspace, TerraformStack, TerraformOutput, RemoteBackend
 from cdktf_cdktf_provider_aws.provider import AwsProvider
 from cdktf_cdktf_provider_aws.instance import Instance
+from cdktf_cdktf_provider_aws.vpc import Vpc
+from cdktf_cdktf_provider_aws.subnet import Subnet
 
 print("Generating")
 
@@ -11,13 +13,32 @@ class MyStack(TerraformStack):
 
         AwsProvider(self, "AWS", region="eu-west-1")
 
+        vpc = Vpc(self, "homedir",
+                  tags={"name": "Juris"},
+                  instance_tenancy="default",
+                  cidr_block="10.0.0.0/16")
+
+        subnet = Subnet(self, "homesubnet",
+                        vpc_id=vpc.id,
+                        cidr_block="10.0.0.0/24",
+                        availability_zone="eu-west-1a",
+                        depends_on=[vpc])
+
         instance = Instance(self, "compute",
                             ami="ami-0766b4b472db7e3b9",
                             instance_type="t2.micro",
+                            subnet_id=subnet.id,
+                            depends_on=[subnet]
                             )
 
-        TerraformOutput(self, "public_ip",
-                        value=instance.public_ip,
+        TerraformOutput(self, "vpc_id",
+                        value=vpc.id,
+                        )
+        TerraformOutput(self, "subnet_id",
+                        value=subnet.cidr_block,
+                        )
+        TerraformOutput(self, "instance_id",
+                        value=instance.id,
                         )
 
 
